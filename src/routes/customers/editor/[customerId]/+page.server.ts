8b249ed697a2708838be4b06cd224feac49992e0
Christian Hosh <104357056+ChristianHosh@users.noreply.github.com>
2023-11-10 14:10:50 +0200

    removed editors

--- src/routes/customers/editor/[customerId]/+page.server.ts
+++ src/routes/customers/editor/[customerId]/+page.server.ts
@@ -1,9 +1,7 @@
-import {fail, redirect} from "@sveltejs/kit";
+import {fail} from "@sveltejs/kit";
 import {HttpClient} from "$lib/core/api/axiosInstance";
 import {AxiosError} from "axios";
 import type {Customer} from "$lib/core/models/customer.model";
-import {customerSchema} from "$lib/core/models/customer.model";
-import type {AxiosResponse} from "axios";
 
 // @ts-ignore
 export async function load(event) {
@@ -28,47 +26,6 @@ export async function load(event) {
 
 export const actions = {
   default: async (event: any) => {
-    const formData = Object.fromEntries(await event.request.formData());
 
-    const customerId = event.url.pathname.split("/")[3];
-
-    let customerRequest = customerSchema.safeParse({
-      fullName: formData.fullName,
-      address: formData.address,
-      phoneNumber: formData.phoneNumber,
-      handle: formData.handle,
-      cityId: Number(formData.cityId),
-    });
-
-    if (!customerRequest.success) {
-      const { fieldErrors: errors } = customerRequest.error.flatten();
-      return fail(400, {
-        data: formData,
-        errors,
-        errorMessage: undefined,
-      });
-    }
-
-    let customerResponse: AxiosResponse<Customer> | undefined;
-    try {
-      customerResponse = await HttpClient.put<Customer>(
-          `/customers/${customerId}`,
-          customerRequest.data,
-          event.locals.user
-      );
-    } catch (e) {
-      if (e instanceof AxiosError) {
-        console.log('error => ', e.response?.data)
-        return fail(e.response?.status || 400, {
-          data: formData,
-          errors: undefined,
-          errorMessage: e.response?.data.message,
-        });
-      }
-      return fail(500)
-    }
-
-    if (customerResponse)
-      throw redirect(307, `/customers/${customerResponse.data.id}`);
-  },
+  }
 };
--- src/routes/customers/editor/[customerId]/+page.svelte
+++ src/routes/customers/editor/[customerId]/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import CustomerEditor from "$lib/ui/editors/CustomerEditor.svelte";
 
   export let data;
-  export let form;
 </script>
-
-<CustomerEditor customer={data.customer} errors={form?.errors} headerError={form?.errorMessage}/>
